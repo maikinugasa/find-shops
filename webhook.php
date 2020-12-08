8945a7411e5b4ee63de9a742185b382bb4713392
maikinugasa <maicjgj1023@yahoo.co.jp>
2020-12-08 14:12:15 +0900

    first commit

---
+++ webhook.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+require_once('./LINEBotTiny.php');
+
+$channelAccessToken = '<BbL/RNtwsxrHigxbAj7pAB3T+nNPo+jNRrXr0S880xsIVZQduWB2D6ota5TwTBa021/n2t1LAkQy2U3i9B2rL6//InN3OY/UTmsfHDOSVyiacE09H7R4QXHlkA5VFCVmvLPahehp6c0y3/R/SpMimgdB04t89/1O/w1cDnyilFU=>';
+$channelSecret = '<97b439c52c6674d778fc4f3707d2e10a>';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage([
+                        'replyToken' => $event['replyToken'],
+                        'messages' => [
+                            [
+                                'type' => 'text',
+                                'text' => $message['text']
+                            ]
+                        ]
+                    ]);
+                    break;
+                default:
+                    error_log('Unsupported message type: ' . $message['type']);
+                    break;
+            }
+			/*
+		case 'location':
+			$location = $bot->get_location();
+			$lat = $location['latitude'];
+			$log = $location['longitude'];
+			$url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=e97210288f59c4af&lat=$lat&lng=$lng";
+			//curlセッションを初期化
+			$ch = curl_init();
+
+			//curlオプションを設定
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+			//curlを実行して、その内容を変数に格納
+			$response = curl_exec($ch);
+
+			//jsonエンコードをPHPの変数に変換する
+			$json = json_decode($response, true);
+
+			curl_close($ch);
+            break;
+        default:
+            error_log('Unsupported event type: ' . $event['type']);
+            break;
+			 */
+    }
+};
