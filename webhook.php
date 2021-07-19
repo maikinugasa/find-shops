12d11cc561314444dc8694f39c1ee26c5328e60f
maikinugasa <maicjgj1023@yahoo.co.jp>
2021-07-19 15:51:49 +0900

    ファイル設定変更

--- webhook.php
+++ webhook.php
@@ -18,33 +18,73 @@
 
 require_once('./LINEBotTiny.php');
 
-$channelAccessToken = '<BbL/RNtwsxrHigxbAj7pAB3T+nNPo+jNRrXr0S880xsIVZQduWB2D6ota5TwTBa021/n2t1LAkQy2U3i9B2rL6//InN3OY/UTmsfHDOSVyiacE09H7R4QXHlkA5VFCVmvLPahehp6c0y3/R/SpMimgdB04t89/1O/w1cDnyilFU=>';
-$channelSecret = '<97b439c52c6674d778fc4f3707d2e10a>';
+$channelAccessToken = 'xxxxx';
+$channelSecret = 'xxxxx';
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
+
+//LINEBotTinyファイル内のparseEventsメソッドで取得したデータを$eventとして回す
 foreach ($client->parseEvents() as $event) {
-    switch ($event['type']) {
-        case 'message':
-            $message = $event['message'];
-            switch ($message['type']) {
-                case 'text':
-                    $client->replyMessage([
-                        'replyToken' => $event['replyToken'],
-                        'messages' => [
-                            [
-                                'type' => 'text',
-                                'text' => $message['text']
-                            ]
-                        ]
-                    ]);
-                    break;
-                default:
-                    error_log('Unsupported message type: ' . $message['type']);
-                    break;
-            }
-            break;
-        default:
-            error_log('Unsupported event type: ' . $event['type']);
-            break;
-    }
+	switch ($event['type']) {
+		case 'message':
+			//event->message->id,type,title,adress,latitude,longitudeという配列の構造
+			$message = $event['message'];
+			//$event->message->type->textやimageやlocationという配列の構造
+			switch ($message['type']) {
+				case 'location': //位置情報が送信された場合は下記処理を実行
+					$lat = $message['latitude'];
+					$lng = $message['longitude'];
+					$url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=e97210288f59c4af&lat=' . $lat . '&lng=' . $lng . '&format=json';
+					//セッション初期化
+					$ch = curl_init();
+					//curlオプションを設定
+					curl_setopt($ch, CURLOPT_URL, $url);
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+					//curlを実行して、その内容を変数に格納
+					$response = curl_exec($ch);
+					//jsonエンコードをPHPの変数に変換する
+					$json = json_decode($response, true);
+					$shops = $json['results']['shop']; //$jsonで取得したデータの配列内のデータを絞り込み
+					//さらにshopの配列内の取り出したいデータを取得
+					foreach ($shops as $key => $value) {
+						$results .= "■";
+						$results .= $value['name'] . "\n";
+						$results .= $value['urls']['pc'] . "\n\n";
+						if ($key == 3) {
+							break;
+						}
+					}
+					//送信元にリプライ
+					$client->replyMessage([
+						'replyToken' => $event['replyToken'],
+						'messages' => [
+							[
+								'type' => 'text',
+								'text' => $results
+							]
+						]
+					]);
+					break;
+				case 'text':
+					$client->replyMessage([
+						'replyToken' => $event['replyToken'],
+						'messages' => [
+							[
+								'type' => 'text',
+								'text' => 'こんにちは！位置情報を送信してお店を検索してみましょう♪'
+							]
+						]
+					 ]);
+					break;
+				default:
+					error_log('Unsupported message type: ' . $message['type']);
+					break;
+			}
+			break;
+		default:
+			error_log('Unsupported event type: ' . $event['type']);
+			break;
+	}
 };
+?>
+
